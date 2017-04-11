f860a3142ab871a7f07be84b27e834ec3ed9bb3d
zyy329 <learnning_zhang@126.com>
2017-04-11 17:19:30 +0800

    第一次提交, 空 maven 项目提交

---
+++ src/main/java/com/zyyApp/netlib/App.java
@@ -0,0 +1,10 @@
+package com.zyyApp.netlib;
+
+/**
+ * Created by zyy on 2017/4/11.
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World");
+    }
+}
